391cb21706ba75f0cb9ce4669f7e3f1142a3ad22
letmewin22 <lkosteckiy5@gmail.com>
2021-10-06 09:27:06 +0300

    add smoothscroll

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <Header />
-    <main>
-      <Nuxt />
-    </main>
-    <Popup />
+  <div id="app">
+    <div id="scroll-container">
+      <Header />
+      <main>
+        <Nuxt />
+      </main>
+      <Popup />
+    </div>
   </div>
 </template>
 
@@ -15,6 +17,21 @@ export default {
   components: {
     Header,
     Popup,
+  },
+
+  async mounted() {
+    const { SmoothScroll } = await import('@emotionagency/smoothscroll')
+    const { raf } = await import('@emotionagency/utils')
+
+    window.ss = new SmoothScroll({
+      mobile: false,
+      breakpoint: 960,
+      passive: false,
+      isFixed: false,
+      friction: 0.07,
+      stepSize: 0.9,
+      raf,
+    })
   },
 }
 </script>
