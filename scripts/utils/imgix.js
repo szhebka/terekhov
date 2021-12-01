65c58bb51cf884d7d3359a1e602d07cdb8d0f4f8
letmewin22 <lkosteckiy5@gmail.com>
2021-12-01 12:35:11 +0200

    change imgix settings

--- scripts/utils/imgix.js
+++ scripts/utils/imgix.js
@@ -2,7 +2,7 @@ import ImgixClient from '@imgix/js-core'
 
 export const imgix = new ImgixClient({
   domain: process.env.IMGIX_DOMAIN || 'terekhov.imgix.net',
-  secureURLToken: process.env.IMGIX_KEY,
+  secureURLToken: process.env.IMGIX_KEY || 'MNN7XeePPsFyC8Ys',
 })
 
 export const imgixTransform = url => imgix.buildURL(url, {})
