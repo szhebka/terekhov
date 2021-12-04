905a101271ae40237d48463c197d3cdf89d0560e
letmewin22 <lkosteckiy5@gmail.com>
2021-12-04 14:10:10 +0200

    change anchor logic

--- components/AnchorLink.vue
+++ components/AnchorLink.vue
@@ -22,8 +22,6 @@ export default {
       const href = anchor
 
       this.$nextTick(() => {
-        window.ss.opts.friction = 1
-        document.body.style.opacity = 0
         setTimeout(() => {
           this.goToAnchor(href)
         }, 2500)
@@ -38,11 +36,9 @@ export default {
 
       const distance = endLocation - startLocation + window.innerHeight / 4
 
-      document.body.style.opacity = 1
       if (window.innerWidth >= 1024) {
         window.ss.state.target = distance - window.innerHeight / 2
         await delayPromise(1000)
-        window.ss.opts.friction = 0.03
       } else {
         $sc.scrollTop = distance - window.innerHeight / 2
       }
